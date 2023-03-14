465f88ee670827a6393495be6d6802de8af8ca75
Quentin Lopes <lopes.quentin16@gmail.com>
2023-03-14 08:56:06 -0400

    initial commit

---
+++ db.js
@@ -0,0 +1,78 @@
+const albums = [{  
+    albumName: "Queen of Me",
+    artist: "Shania Twain",
+    publishDate: "February 3, 2023",
+    imgURL: "https://cdn2.albumoftheyear.org/375x/album/563931-queen-of-me.jpg",
+    songGenres: ["Country Pop","Pop Rock"]
+  },
+  {  
+    albumName: "Don't Get Too Close",
+    artist: "Skrillex",
+    publishDate: "February 18, 2023",
+    imgURL: "https://cdn2.albumoftheyear.org/375x/album/563931-queen-of-me.jpg",
+    songGenres: ["Pop Rap","Trap", "Future Bass"]
+  },
+  {
+    albumName: "Great Is He",
+    artist: "Popcaan",
+    publishDate: "January 27, 2023",
+    imgURL: "https://cdn2.albumoftheyear.org/200x/album/592147-great-is-he-1.jpg",
+    songGenres: "Reggae"
+  },
+  {
+    albumName: "Get Real",
+    artist: "CVC",
+    publishDate: "January 13, 2023",
+    imgURL: "https://cdn2.albumoftheyear.org/200x/album/558323-get-real.jpg",
+    songGenres: ["Alternative/Indie", "Rock"]
+  },
+  {
+    albumName: "On The Prowl",
+    artist: "Steel Panther",
+    publishDate: "February 24, 2023",
+    imgURL: "",
+    songGenres: ["Rock"]
+  },
+  {
+    albumName: "Gloria",
+    artist: "Sam Smith",
+    publishDate: "January 27, 2023",
+    imgURL: "",
+    songGenres: ["Pop Soul"]
+  },
+  {
+    albumName: "TRUSTFALL",
+    artist: "P!nk",
+    publishDate: "February 17, 2023",
+    imgURL: "",
+    songGenres: ["Pop","Adult Contemporary"]
+  },
+  {
+    albumName: "Five Easy Hot Dogs",
+    artist: "Mac DeMarco",
+    publishDate: "January 20, 2023",
+    imgURL: "",
+    songGenres: ["Soft Rock","Bedroom Pop"]
+  },
+  {
+    albumName: "Amelia",
+    artist: "Mimi Webb",
+    publishDate: "March 3, 2023",
+    imgURL: "",
+    songGenres: ["Pop"]
+  },
+  {
+    albumName: "",
+    artist: "",
+    publishDate: "",
+    imgURL: "",
+    songGenres: ["",""]
+  },
+  {
+    albumName: "",
+    artist: "",
+    publishDate: "",
+    imgURL: "",
+    songGenres: ["",""]
+  },
+]
