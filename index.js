312d27e9c634e6db80f92de24aa55199b8d78450
Quentin Lopes <lopes.quentin16@gmail.com>
2023-03-14 11:24:48 -0400

    integrated referance links and images

--- index.js
+++ index.js
@@ -7,3 +7,17 @@ app.use(express.static('public'));
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
 });
+
+app.get('/albums', (req, res) => {
+    const htmlData = `<h1>Top 25</h1>
+                    <ul>
+                    <li><a href="/albums/0">lil peep</a></li>
+                    </ul>`
+    res.send(htmlData);
+});
+
+app.get('/albums/:id', (req, res) => {
+    const {id} = req.params;
+    const htmlData = `<h1>here they are</h1>
+                        <a href="">${id}</a>` 
+});
